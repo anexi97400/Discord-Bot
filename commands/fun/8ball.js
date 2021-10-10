2a764c33c85691e0b80cabe5e1cfadfe4120d035
squarfiuz <70509576+squarfiuz@users.noreply.github.com>
2021-10-10 23:18:47 +0200

    Update 8ball.js

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,34 +1,17 @@
-const Discord = require('discord.js');
 const answersList = require('../../config.json')
-module.exports ={
-    name: "8ball",
-    description: "Ask something to the magic 8ball.",
-    category: "fun",
+
+module.exports = {
+    name: '8ball',
+    description: 'Ask something to the magic 8ball.',
+    category: 'fun',
     run: async(client, message, args) => {
+	if (!args.length) return message.channel.send('You need to ask the magic 8ball something, dude.');
 
-
- let question = args.slice(1).join(" ");
-        
-        if (!question){
-          message.channel.send("You need to ask the magic 8ball something, dude.");
-          return;
-        }       
-      
-
-
-
-        const ballEmbed = new Discord.MessageEmbed()
-	.setTitle('Ask the magic 8ball')
-	.setDescription(`**You shake your 8ball and think hard, yet simple.**\n:8ball: You look into your 8ball and see ` + random(answersList) + ".");
-
-
-
-
-       message.channel.send(ballEmbed)
+	message.channel.send({
+		embed: {
+			title: 'Ask the magic 8ball',
+			description: '**You shake your 8ball and think hard, yet simple.**\n:8ball: You look into your 8ball and see ' + answers[Math.floor(Math.random() * answers.length)] + '.'
+		}
+	});
     }
-}
-
-function random(answers){
-  const chooseanswer = answers[Math.floor(Math.random() * answers.length)];
-  return chooseanswer;
 }
