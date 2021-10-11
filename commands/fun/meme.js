8178423f2942889bc1eb26fab4c9a8545114d7a2
squarfiuz <70509576+squarfiuz@users.noreply.github.com>
2021-10-11 16:53:28 +0200

    👾 Optimization

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,14 +1,12 @@
-const { Random } = require("something-random-on-discord")
+const { Random } = require('something-random-on-discord');
 
 module.exports = {
-  name: "meme",
-  category: "fun",
-
-  description: "Get Fresh meme :D",
+  name: 'meme',
+  category: 'fun',
+  description: 'Get Fresh meme :D',
   run: async (client, message, args) => {
+    const data = await Random.getMeme();
 
-    let data = await Random.getMeme()
-    message.channel.send(data)
-
+    message.channel.send(data);
   }
-}
+};
