7f80a9b1dceec6a4b259ed37296571cd4cd07ad7
squarfiuz <70509576+squarfiuz@users.noreply.github.com>
2021-10-11 07:07:57 +0200

    Update acscii.js

--- commands/fun/acscii.js
+++ commands/fun/acscii.js
@@ -1,16 +1,14 @@
 const figlet = require('figlet');
 
 module.exports = {
-    name: "ascii",
-    description: "Converts text to ascii",
-    category: "fun",
-    run: async(client, message, args) => {
-        if(!args[0]) return message.channel.send('Please provide some text');
+    name: 'ascii',
+    description: 'Converts text to ascii',
+    category: 'fun',
+    run: async (client, message, args) => {
+        if(!args.length) return message.channel.send('Please provide some text');
 
-        msg = args.join(" ");
-
-        figlet.text(msg, function (err, data){
-            if(err){
+        figlet.text(args.join(' '), function (err, data){
+            if (err) {
                 console.log('Something went wrong');
                 console.dir(err);
             }
