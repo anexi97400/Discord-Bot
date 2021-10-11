427a7689981c1a15476090cfc61bbaeefbe70763
squarfiuz <70509576+squarfiuz@users.noreply.github.com>
2021-10-11 16:39:21 +0200

    👾 Optimisation

--- commands/fun/translate.js
+++ commands/fun/translate.js
@@ -1,21 +1,21 @@
 const translate = require('@iamtraction/google-translate')
 
-module.exports= {
-    name : 'translate',
-    category: "fun",
-    description: "Translate a non-english text in English",
-    usage: ".translate <non-english text>",
+module.exports = {
+    name: 'translate',
+    category: 'fun',
+    description: 'Translate a non-english text in English',
+    usage: '.translate <non-english text>',
    /**
    * @param {Client} client
    * @param {Message} message
    * @param {String[]} args
    */
-    run : async(client, message, args) => {
+    run: async(client, message, args) => {
 
-    const query = args.join(" ");
-    if (!query) return message.reply("Please specify a text to translate");
+    if (!args.length) return message.reply('Please specify a text to translate');
 
-    const translated = await translate(query, {to : 'en'});
+    const translated = await translate(args.join(' '), { to : 'en' });
     message.channel.send(translated.text)
         
- } }
+    } 
+};
