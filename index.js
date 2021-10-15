f110cdc62a5bed95a8cf7ec206e750ee16be8c6d
slow <lilian.filoche2007@gmail.com>
2021-10-15 20:11:14 +0200

    new

--- index.js
+++ index.js
@@ -1,99 +1,41 @@
-const { Client, Collection } = require("discord.js"); 
-const client = new Client({ disableEveryone: true });
-require('discord-buttons')(client);
+const { Collection } = require("discord.js");
+const { DiscordBot } = require('./base/DiscordBot')
 
-const { prefix } = require('./events/message');
+const client = new DiscordBot({
+    fetchAllMembers: !0,
+    autoReconnect: !0,
+    messageCacheMaxSize: 20,
+    partials: ["MESSAGE", "CHANNEL", "GUILD_MEMBER", "REACTION", "GUILD_VOICE_STATES"],
+    intents: [
+        "GUILD_MEMBERS",
+        "GUILDS",
+        "GUILD_VOICE_STATES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_BANS",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+        "DIRECT_MESSAGES",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING",
+        "GUILD_MESSAGE_REACTIONS",
+    ],
+    allowedMentions: {
+        parse: ["users", "roles"]
+    },
+});
+module.exports = client;
 
-const db = require('quick.db');
-const { GiveawaysManager } = require('discord-giveaways');
-
+// Global Variables
 client.commands = new Collection();
-client.aliases = new Collection();
+client.slashCommands = new Collection();
+client.config = require("./config.json");
 
-['command', 'events'].forEach(handler => {
-  require('./handlers/' + handler)(client);
-});
+// Initializing the project
+require("./base")(client);
 
-//GIVEAWAY//
-if (!db.get("giveaways")) db.set("giveaways", []);
-
-const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
-
-    // This function is called when the manager needs to get all the giveaway stored in the database.
-    async getAllGiveaways(){
-        // Get all the giveaway in the database
-        return db.get("giveaways");
-    }
-
-    // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
-    async saveGiveaway(messageID, giveawayData){
-        // Add the new one
-        db.push("giveaways", giveawayData);
-        // Don't forget to return something!
-        return true;
-    }
-
-    async editGiveaway(messageID, giveawayData){
-        // Gets all the current giveaways
-        const giveaways = db.get("giveaways");
-        // Remove the old giveaway from the current giveaways ID
-        const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID);
-        // Push the new giveaway to the array
-        newGiveawaysArray.push(giveawayData);
-        // Save the updated array
-        db.set("giveaways", newGiveawaysArray);
-        // Don't forget to return something!
-        return true;
-    }
-
-    // This function is called when a giveaway needs to be deleted from the database.
-    async deleteGiveaway(messageID){
-        // Remove the giveaway from the array
-        const newGiveawaysArray = db.get("giveaways").filter((giveaway) => giveaway.messageID !== messageID);
-        // Save the updated array
-        db.set("giveaways", newGiveawaysArray);
-        // Don't forget to return something!
-        return true;
-    }
-
-};
-
-// Create a new instance of your new class
-const manager = new GiveawayManagerWithOwnDatabase(client, {
-    storage: false,
-    updateCountdownEvery: 5000,
-    default: {
-        botsCanWin: false,
-        embedColor: "#2C2F33",
-        embedColorEnd: "#2C2F33",
-        reaction: "🎉"
-    }
-});
-
-
-client.giveawaysManager = manager;
-// We now have a client.giveawaysManager property to manage our giveaways!
-
-client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
-    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
-});
-
-client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
-    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
-});
-
-
-
-// Listen on host 8080 //
-const http = require('http');
-
-const hostname = '127.0.0.1';
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(8080);
-client.login(process.env.TOKEN);
+client.login(client.config.token);
